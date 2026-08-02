43329ffba4114d879eee97cd29e479086b8c63ab
MD FARHAN ALI <farhanali4953@gmail.com>
2026-08-02 12:51:48 +0530

    feat(home): add reservation call-to-action section

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -4,10 +4,12 @@ import { WhyChooseUs } from "../../components/why-choose-us/why-choose-us";
 import { PopularDishes } from '../../components/popular-dishes/popular-dishes';
 import { SpecialOffer } from "../../components/special-offer/special-offer";
 import { OurChefs } from "../../components/our-chefs/our-chefs";
+import { Gallery } from "../../components/gallery/gallery";
+import { ReservationCta } from "../../components/reservation-cta/reservation-cta";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, WhyChooseUs, PopularDishes, SpecialOffer, OurChefs],
+  imports: [Hero, WhyChooseUs, PopularDishes, SpecialOffer, OurChefs, Gallery, ReservationCta],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
